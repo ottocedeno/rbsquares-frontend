2c2c467fcb883b58e30d451bb9e2023fa29be9b6
ottocedeno <otto@creativespaceclub.com>
2020-09-07 19:55:27 -0400

    Added Signup Class to handle user sign up

--- index.js
+++ index.js
@@ -69,12 +69,13 @@ class Login {
     const signUpLink = document.createElement("a");
 
     signUpElement.className = "center-text";
-    signUpElement.innerText = "Don't have an account?";
+    signUpElement.innerText = "First time playing?";
 
     signUpLink.innerText = " Sign Up";
     signUpLink.addEventListener("click", (e) => {
       e.preventDefault();
-      console.log("load the sign up page");
+      this.removeLoginCard();
+      SignUp.renderSignUpCard("animate__bounceInRight");
     });
 
     signUpElement.appendChild(signUpLink);
@@ -124,7 +125,118 @@ class Login {
 
   static removeLoginCard() {
     const loginContainer = document.getElementById("login");
-    loginContainer.remove();
+    loginContainer.classList.remove("animate__fadeInUp");
+    loginContainer.classList.add("animate__bounceOutLeft");
+    setTimeout(function () {
+      loginContainer.remove();
+    }, 1000);
+    // loginContainer.remove();
+  }
+}
+
+class SignUp {
+  static renderSignUpCard(animation) {
+    const signUpContainer = document.createElement("div");
+    signUpContainer.id = "signup";
+    signUpContainer.classList.add("card", "animate__animated", animation);
+
+    const signUpForm = document.createElement("form");
+    const inputUsername = document.createElement("input");
+    const inputPassword = document.createElement("input");
+    const submitBtn = document.createElement("button");
+
+    inputUsername.id = "username";
+    inputUsername.type = "text";
+    inputUsername.placeholder = "username";
+
+    inputPassword.id = "password";
+    inputPassword.type = "password";
+    inputPassword.placeholder = "password";
+
+    submitBtn.className = "btn-primary";
+    submitBtn.innerText = "SIGNUP";
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.sendSignUpData();
+    });
+
+    signUpForm.append(
+      inputUsername,
+      inputPassword,
+      submitBtn,
+      this.renderLinkToLogin()
+    );
+    signUpContainer.append(signUpForm);
+
+    APP.container.append(signUpContainer);
+  }
+
+  static renderLinkToLogin() {
+    const loginElement = document.createElement("p");
+    const loginLink = document.createElement("a");
+
+    loginElement.className = "center-text";
+    loginElement.innerText = "Have an account?";
+
+    loginLink.innerText = " Login";
+    loginLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.removeSignUpCard();
+      Login.renderLoginCard("animate__bounceInRight");
+    });
+
+    loginElement.appendChild(loginLink);
+
+    return loginElement;
+  }
+
+  static formData() {
+    const userName = document.getElementById("username");
+    const password = document.getElementById("password");
+
+    const user = {
+      username: userName.value,
+      password: password.value,
+    };
+
+    return user;
+  }
+
+  static sendSignUpData() {
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        user: this.formData(),
+      }),
+    })
+      .then((r) => r.json())
+      .then((obj) => {
+        if (obj.user) {
+          console.log(obj);
+          APP.setSession(obj.jwt);
+          APP.clearDOM();
+          APP.container.innerHTML = "<p>Loading the Game</p>";
+        } else {
+          this.renderError(obj);
+        }
+      });
+  }
+
+  static renderError(error) {
+    alert(error["message"]);
+  }
+
+  static removeSignUpCard() {
+    const signUpContainer = document.getElementById("signup");
+    signUpContainer.classList.remove("animate__fadeInUp");
+    signUpContainer.classList.add("animate__bounceOutLeft");
+    setTimeout(function () {
+      signUpContainer.remove();
+    }, 1000);
   }
 }
 
