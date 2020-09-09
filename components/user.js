deae4443889f03eb766b6b45d576e61b758c0ffd
ottocedeno <otto@creativespaceclub.com>
2020-09-08 20:35:50 -0400

    created and linked User model JS

---
+++ components/user.js
@@ -0,0 +1,8 @@
+class User {
+  constructor(username, balance, winstreak, userId) {
+    this.username = username;
+    this.balance = balance;
+    this.winstreak = winstreak;
+    this.userId = userId;
+  }
+}
